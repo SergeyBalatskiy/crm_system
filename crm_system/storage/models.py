a9ccf9689582c986c248682ee8601ea5597cdb79
Sergey <balaczkiysergeys@gmail.com>
2026-09-02 12:41:42 +0300

    create new DB in storage + create fields for add new items, make migrations + make migrate

--- crm_system/storage/models.py
+++ crm_system/storage/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your models here.
+class StorageInfo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='storage')
+    individual_code = models.IntegerField(max_length=100) # Код
+    name_product = models.CharField(max_length=120)
+    quantity_at_the_purchase = models.IntegerField(max_length=100) # Количество на момент закупки
+    supplier = models.CharField(max_length=120, blank=True) # Поставщик
+    buy_price = models.IntegerField(max_length=100) # Стоимость закупки (1 шт.)
+    retail_price = models.IntegerField(max_length=100) # Цена для продажи (1 шт.)
+    minimum_items_for_notification = models.IntegerField(max_length=100, default=10) # Минимальное количество для напоминания
+    remainder = models.IntegerField(max_length=100) # Остаток на складе
+
+    def __str__(self):
+        return f'Название: {self.name_product}, Кол-во: {self.remainder}, связан с {self.user.get_full_name()}'
--- crm_system/storage/views/main_storage.py
+++ crm_system/storage/views/main_storage.py
@@ -15,54 +15,6 @@ class StorageCustomView(TemplateView):
     template_name = 'storage/main-storage.html'
 
     def post(self, request, *args, **kwargs):
-
-        # cur_doc = request.POST.get('current_document')
-
-        # # Для получения названия документа
-        # document_name = {
-        #     'garanty_doc': 'Акт о гарантии',
-        #     'adoption_doc': 'Акт о принятии',
-        #     'cancell_doc': 'Акт о отказе',
-        #     'complete_doc': 'Акт о выполненных работах'
-        # }
-        # data_name = document_name.get(cur_doc)
-        # document_data = DocumentInformation.objects.get(user = self.request.user, name = data_name)
-
-        # # Получаю .txt название документа
-        # document_name_txt = {
-        #     'garanty_doc': 'garanty.txt',
-        #     'adoption_doc': 'addoption.txt',
-        #     'cancell_doc': 'cancell.txt',
-        #     'complete_doc': 'complete.txt'
-        # }
-        # txt_name = document_name_txt.get(cur_doc)
-
-        # # Ищу "место" где у меня находится мой .txt файл
-        # # Когда указывается путь к файлу который находится в статике, необходимо писать путь самого приложения + папку которую нужно найти
-        # file_path_txt = finders.find(f'auth_registration/txt/{txt_name}')
-        
-        # # Открываю, записываю в переменную, внедряю текущий документ в базу данных и сохраняю:
-        # with open(file_path_txt, 'r', encoding='utf-8') as file:
-        #     clear_document = file.read()
-        #     document_data.content = clear_document
-        #     document_data.save()
-
-        # # Отдаю контент (Размету документа)
-        # content = document_data.content
-
-        # # Название документа на eng
-        # document_selected = cur_doc
-
-        # # ОТДАЮ И название, и текст и сообщение
-        # response = render(
-        #     request, 
-        #     self.documents_form, 
-        #     {'content': content, 'cur_doc': document_selected}, 
-        #     status=200
-        # )
-        # response['HX-Trigger'] = 'backup_success' 
-
-        # return response
         ...
         
     def get(self, request, *args, **kwargs):
